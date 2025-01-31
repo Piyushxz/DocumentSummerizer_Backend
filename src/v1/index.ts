13dea650d3693e946b6a971cf6af893d7727215f
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2025-01-31 19:34:39 +0530

    error fix

--- src/v1/index.ts
+++ src/v1/index.ts
@@ -23,23 +23,11 @@ v1Router.get('/',(req,res)=>{
     res.json("Hey")
 })
 
-const serviceAccount = {
-    "type": "service_account",
-    "project_id": "aerobic-stream-448606-f3",
-    "private_key_id": "c3e01df3b14aa65caa193132794bc2561f061eb7",
-    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQC2CEIO5EuXevlS\nOong2FK7vCfko5G1o87H5uUz6Am1GM1v+oQMlSaosY0AOYjFZJQj6UoJxMBzwTDC\n67A5nTBmFoDIvP501HlptT4rdbu038rQdxtNZAtrtJtX2RZtm0o9fyAhQDnlTHPA\nz/houcDtl/iwqSYh4JYqm1RJNjdGBJvuyoR3/HknRlxQwKcZ1LL/oDrlIO2lFoCi\nDvcl8yvlXha1dPlx0nevZx0Qs3DK7D47ouMAlSYsZMWtbDpLk1i+wPZ7Car9picj\nWqwe4YtUiiuRBMQh8t4Y5r5aQXXlChTecBIZB9tCjhTDSqm9ZnSTPtD+QtZJS8NY\nrAkv2jYrAgMBAAECggEAAUi+S+PuaJYp1huq8gF0q/W1g6u9ERP5FyNDhgHit6VX\nnewJrFg+jTdiwJEqY8r41O/+e/Gyn6D56UpWzu477/WxhN4lQc5CK1A++7Yg6F8Tl\nuwzBY41gyz9jQn5kkHINAbFjj6MoBWcXAKA292jVZJey3cQuYa93lRCyyoVtubph\nhZv7rJQ9zn2O1CXP80/w1tUfUUpRdR1L5/W3rcLbDdsnlCujjD7qV1mmTwKQ7vMp\nQFDThI2FBXRZSMkcM8M2altQkDFibdW+PfBAHlY6S4TKBXXTy3fB2R6FKor0H4aM\nwiHT1AIg/bQYXvjK7F+2kEi4MRrQg3n3Y/JjI3uzQQKBgQD3WV4naBUaUMbmSE7N\nd8AEKH0+bq0gy34zj0fTIvqEA8MnOTtziaL2PGGkqHbLCKoXqEt2T/Pyir5yC7+y\nmVfjJHNcronUpPhwF04O+rsX2nEocCrIibKqKlyqhbrDAkaQfzSDGfRHSCeSfCHw\nQ5TqlrmwzVUqar2sgppomSAqIQKBgQC8ZhP7TB7luSJCs18nc/+O2b3xWDMnyduo\neofZLgyxFbkmH3MUpO4G7oEjI6VUoYl35FNFiRxcntxdtWOhm0zlIzg8H/RTjHKc\nF2eTZyRwqgoh0Ecbi/KxUu03+KLBBFonhjjP3ytjWB1gph9ekx8X7E46rp4l5O8Q\nDh3yPasOywKBgDhI1L0A2TR4xxnlwFDO/Bm2IPHQ+4Jn69rNstlfr0PVG1ZUlquu\n2S9RBCMU1ptS6GmjuTA991PssHOBKuj4LeCZDcs8SE7kD0hqdS/BbEt8QJ2kEIg7\nlTVVmGqRLbNsVCRTXd6rTEPgLmkN6CclKcDL9Ys+3i9dxLV5rqs4lPIhAoGAPVEz\n26xQug/hWLgslhio6oNv0KcWLzMBrPoEUOkt9EzPr9q4h9WOKu9hVGB7pOnWZhEI\nES+o7XQ+4LjyrlxvUHyABwGXccHaY3cynMULFSgimBLDsfGAkbodvwPLZOrXtNl4\nkB9gbbr2BMjMmOllS1H7vBmtG0RBkoYltPvhOrUCgYBUEXWGoMvx5CrVrRrlSztj\n/XQSJme+4P8UHSt4TELwuAzqaLKuvWl+WCLZFYxQxVNPrZjGrJLzweD3H23SUvKt\nLYQVu8IQB2+VJGwEJ64vdTlg4RsMHdlCAqtr9MeIQxP8+ybTix4gikXo6mC9U2Or\nLGgt/blcPy3ZGCagLK8fnA==\n-----END PRIVATE KEY-----\n",
-    "client_email": "piyush@aerobic-stream-448606-f3.iam.gserviceaccount.com",
-    "client_id": "107043059670807391872",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/piyush%40aerobic-stream-448606-f3.iam.gserviceaccount.com",
-    "universe_domain": "googleapis.com"
-};
+
 
 const auth = new GoogleAuth({
     scopes: ['https://www.googleapis.com/auth/cloud-platform'],
-    credentials :serviceAccount
+    credentials :process.env.GOOGLE_APPLICATION_CREDENTIALS
 });
 
 const llm = new ChatVertexAI({
